96fe2d29bf6a33cd67e14ff76d41e0d1f513223a
Johnny Richardson <johnny.c.rich@gmail.com>
2018-03-29 16:17:45 -0400

    Fixes as per #14, #15, #13.

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { JsonService } from '../json.service';
 import { fadeInAnimation } from '../_animations/fade';
@@ -8,7 +8,8 @@ import { fadeInAnimation } from '../_animations/fade';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
   animations: [fadeInAnimation],
-  host: { '[@fadeInAnimation]': '' }
+  host: { '[@fadeInAnimation]': '' },
+  encapsulation: ViewEncapsulation.None
 })
 export class AboutComponent implements OnInit {
 
